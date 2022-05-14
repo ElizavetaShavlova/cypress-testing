9d2f7cc7439a05f030e1540fbd8c77364ece5fab
ElizavetaShavlova <eliza260926@gmail.com>
2022-05-14 18:22:22 +0300

    add test for verification of label of each link in main menu

--- cypress/integration/specs/home.spec.js
+++ cypress/integration/specs/home.spec.js
@@ -14,13 +14,42 @@ describe ('Home tests', () => {
         cy.url().should("include", "get-started");
     });
     it ('get the text of the heading and assert the value', () => {
-        //get the text
-        cy.get("h1.elementor-heading-title").should($heading => {
+        //get the text and assert class name
+        cy.get("h1.elementor-heading-title")
+        .should($heading => {
             expect($heading.text()).to.eq("Think different. Make different.");
-        });
+        })
+        .and("have.class", "elementor-size-default");
     });
     it ('verify of the text of the 1-st menu link item', () => {
         cy.get("#primary-menu").find("li").first().should("have.text", "Home");
+    });
+    it('get the length and verify the label (using wrap) of each link in main menu', () => {
+        const menuLinksText = [
+            "Home",
+            "About",
+            "Shop",
+            "Blog",
+            "Contact",
+            "My account",
+        ];
+        cy.get("#primary-menu [id*=menu-item]").should("have.length", 6);
+        cy.get("#primary-menu [id*=menu-item]").each((item, index, list) => {
+            cy.wrap(item).should("contain.text", menuLinksText[index]);
+        })
+    });
+    it.skip('verify the label (using $) of each link in main menu', () => {
+        const menuLinksText = [
+            "Home",
+            "About",
+            "Shop",
+            "Blog",
+            "Contact",
+            "My account",
+        ];
+        cy.get("#primary-menu [id*=menu-item]").each((item, index, list) => {
+            expect(Cypress.$(item).text()).to.eq(menuLinksText[index]);
+        })
     });
 
 });
